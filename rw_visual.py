5155dd8f1c02a033e03b38374391ef6ed141b05f
owzi <frolle@inf.ufpel.edu.br>
2021-01-13 02:54:01 -0300

    exercicios do livro

--- rw_visual.py
+++ rw_visual.py
@@ -2,13 +2,20 @@ import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
 
- # cri  random_walk
-rw = RandomWalk()
-rw.fill_walk() 
-
-plt.style.use('classic')
-fig , ax = plt.subplots()
-
-ax.scatter(rw.x_values, rw.y_values, s = 15)
-
-plt.show()
+while True:
+	 # cri  random_walk
+	rw = RandomWalk()
+	rw.fill_walk() 
+	
+	plt.style.use('classic')
+	fig , ax = plt.subplots()
+	
+	ax.scatter(rw.x_values, rw.y_values, s = 15)
+	
+	plt.show()
+	
+	# pergunta se deve ser gerado outra rw
+	keep_running = input("Make anither Walk? (y/n): ")
+	
+	if keep_running == 'n':
+		break
