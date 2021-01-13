b41121e8227bc3e8dc63cb6d71058e08e6e57992
owzi <frolle@inf.ufpel.edu.br>
2021-01-13 03:17:27 -0300

    exercicios do livro

--- rw_visual.py
+++ rw_visual.py
@@ -5,18 +5,22 @@ from random_walk import RandomWalk
 
 while True:
 	 # cri  random_walk
-	rw = RandomWalk()
+	rw = RandomWalk(50_000)
 	rw.fill_walk() 
 	
 	plt.style.use('classic')
-	fig , ax = plt.subplots()
+	fig , ax = plt.subplots(figsize=(19, 9), dpi=128)
 	point_numbers = range(rw.num_points)
-	ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s = 15)
+	ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s = 1)
 	
 	# marca o inicio e o fim
 	ax.scatter(0, 0, c='green', edgecolor='none', s=1000)	
 	ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolor='none', s=1000)
 	
+	 # desabilita os eixos
+	ax.get_xaxis().set_visible(False)
+	ax.get_yaxis().set_visible(False) 
+	 	 
 	plt.show()
 	
 	# pergunta se deve ser gerado outra rw
