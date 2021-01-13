7e374dac1c642b1d90096ebdaa42bdc5cf84ce79
owzi <frolle@inf.ufpel.edu.br>
2021-01-13 03:07:14 -0300

    exercicios do livro

--- rw_visual.py
+++ rw_visual.py
@@ -13,6 +13,10 @@ while True:
 	point_numbers = range(rw.num_points)
 	ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s = 15)
 	
+	# marca o inicio e o fim
+	ax.scatter(0, 0, c='green', edgecolor='none', s=1000)	
+	ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolor='none', s=1000)
+	
 	plt.show()
 	
 	# pergunta se deve ser gerado outra rw
